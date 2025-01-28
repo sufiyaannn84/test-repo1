c7ff0f471d08f706d5a9f0b2d63ed5ff50b2bdc3
dev1 <dev@mail.com>
2025-01-28 23:23:03 +0530

    dev1 update

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
-class hello() {
-	public static void main()
+class hello()    #from dev1
+{
+	public static void main()       #from dev1
 	{
-
+             system.out.println("hello update from dev1"): 
 	}
 
 }
